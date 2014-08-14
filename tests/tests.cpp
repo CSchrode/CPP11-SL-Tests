08ee05c0d7f8c663bee158fafaf0b50629482c2f
Cameron Schrode <cameron.schrode@wadsworthlibrary.com>
2014-08-14 17:52:58 -0400

    Swapped out tests for more complex and descriptive ones.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,29 +15,34 @@ int main()
     Services::initialize();
 
     CommandRegistry Test;
-    LogEngine* Null1 = new NullLogEngine;
-    std::shared_ptr<LogEngine> Null2(new NullLogEngine);
+    std::shared_ptr<LogEngine> Null1(new ConsoleLogging);
+    std::shared_ptr<LogEngine> Null2(new FileLogging);
     std::shared_ptr<LogEngine> Null3(new NullLogEngine);
-    Test.push_back("null", Null1);
-    Test.push_back("null_", Null2);
-    Test.push_back("null-", Null3);
+    Test.push_back("console", Null1);
+    Test.push_back("file", Null2);
+    Test.push_back("null", Null3);
 
     std::deque<std::string> Results;
 
-    LogLocator::provide(Test.GetLogEngine("null"));
+    LogLocator::provide(Test.GetLogEngine("console"));
     LogLocator::GetLogEngine()->Log("Test 1", Results);
-    assert(Results.at(0) == "Null Log Engine");
+    assert(Results.at(0) == "\"Test 1\" added to log.");
 
-    LogLocator::provide(Test.GetLogEngine("null-"));
+    LogLocator::provide(Test.GetLogEngine("file"));
     LogLocator::GetLogEngine()->Log("Test 2", Results);
-    assert(Results.at(1) == "Null Log Engine");
+    assert(Results.at(1) == "\"Test 2\" added to log.");
 
-    LogLocator::provide(Test.GetLogEngine("null_"));
+    LogLocator::provide(Test.GetLogEngine("null"));
     Services::GetLogEngine()->Log("Test 3", Results);
     assert(Results.at(2) == "Null Log Engine");
 
-    LogLocator::provide(Test.GetLogEngine("null"));
-    Services::GetLogEngine()->Log("Test 4", Results);
+    LogLocator::provide(Test.GetLogEngine("non-existant"));
+    Services::GetLogEngine()->Log("Test 1", Results);
     assert(Results.at(3) == "Null Log Engine");
+
+    LogLocator::provide(Test.GetLogEngine("file"));
+    LogLocator::GetLogEngine()->Log("Test 2", Results);
+    assert(Results.at(4) == "\"Test 2\" added to log.");
+
     return 0;
 }
